d8c6d71cbc60dd251ce323760ccd49e523b54f98
antoniosaleme <kilorunner@protonmail.com>
2024-09-30 15:11:53 +0200

    feat: create filter panel

---
+++ src/constants/languages.ts
@@ -0,0 +1,26 @@
+export const LANGUAGES = [
+    {
+        name: 'English',
+        code: 'en',
+    },
+    {
+        name: 'Spanish',
+        code: 'es',
+    },
+    {
+        name: 'French',
+        code: 'fr',
+    },
+    {
+        name: 'German',
+        code: 'de',
+    },
+    {
+        name: 'Italian',
+        code: 'it',
+    },
+    {
+        name: 'Portuguese',
+        code: 'pt',
+    },
+]
